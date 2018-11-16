a8e77dc317013d6b370a3ed49e482805b20fbafb
Jinji Zhang <362@holbertonschool.com>
2018-11-16 00:42:55 +0000

    betty style check fix

--- div.c
+++ div.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * div - divides the second top element of the stack
+ * _div - divides the second top element of the stack
  * by the top element of the stack
  * @stack: address to pointer of top of the stack
  * @line_number: line number of monty bytecode file
@@ -9,17 +9,17 @@
 void _div(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
-        int value, len = 0;
+	int value, len = 0;
 
-        for (tmp = *stack; tmp; tmp = tmp->next)
-                len++;
-        if (len < 2)
-        {
-                fprintf(stderr, "L%u: can't div, stack too short\n",
-                        line_number);
-                clear_stack(stack);
-                exit(EXIT_FAILURE);
-        }
+	for (tmp = *stack; tmp; tmp = tmp->next)
+		len++;
+	if (len < 2)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n",
+			line_number);
+		clear_stack(stack);
+		exit(EXIT_FAILURE);
+	}
 	value = (*stack)->n;
 	if (value == 0)
 	{
