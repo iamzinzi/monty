422067c7661bbae34e28e33015ecbf53986113dc
Jinji Zhang <362@holbertonschool.com>
2018-11-16 00:44:47 +0000

    betty style fix

--- mul.c
+++ mul.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * mul - multiplies the second top element of the stack
+ * _mul - multiplies the second top element of the stack
  * with the top element of the stack
  * @stack: address to pointer of top of the stack
  * @line_number: line number of monty bytecode file
@@ -9,16 +9,16 @@
 void _mul(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
-        int value, len = 0;
+	int value, len = 0;
 
-        for (tmp = *stack; tmp; tmp = tmp->next)
-                len++;
-        if (len < 2)
-        {
-                fprintf(stderr, "L%u: can't mul, stack too short\n",
-                        line_number);
-                clear_stack(stack);
-                exit(EXIT_FAILURE);
+	for (tmp = *stack; tmp; tmp = tmp->next)
+		len++;
+	if (len < 2)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n",
+			line_number);
+		clear_stack(stack);
+		exit(EXIT_FAILURE);
 	}
 	value = (*stack)->n;
 	pop(stack, line_number);
